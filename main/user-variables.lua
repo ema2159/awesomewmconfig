32d7ee209ca190681d121364326105648296b73b
Emmanuel Bustos <ema2159@gmail.com>
2023-04-29 16:51:09 +0300

    clean: Remove wallpaper code. Mod: Set Kitty as default terminal

--- main/user-variables.lua
+++ main/user-variables.lua
@@ -7,17 +7,13 @@ local home = os.getenv("HOME")
 local _M = {
 	-- This is used later as the default terminal and editor to run.
 	-- terminal = "xterm",
-	terminal = "tilix",
+	terminal = "kitty",
 	-- Default modkey.
 	-- Usually, Mod4 is the key with a logo between Control and Alt.
 	-- If you do not like this or do not have such a key,
 	-- I suggest you to remap Mod4 to another key using xmodmap or other tools.
 	-- However, you can use another modifier like Mod1, but it may interact with others.
 	modkey = "Mod4",
-
-	-- user defined wallpaper
-	wallpaper = home .. "/Pictures/ArtWP/dm1ajryp0lv81.jpg",
-	--wallpaper = home .. "/Pictures/your-wallpaper-here.jpg",
 }
 
 return _M
